051ab5d1905de7397bf3ba33e4e3a88e82697fcf
Sgian24 <93100806+Sgian24@users.noreply.github.com>
2024-10-30 14:31:45 -0400

    Remove and replace null characters from event descriptions.

--- src/Components/Listing.js
+++ src/Components/Listing.js
@@ -1,44 +1,74 @@
-import { useEffect } from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import { useEffect, useState, useRef } from "react";
+import {Container, Row, Col, Modal} from "react-bootstrap";
+
 
 const Listing = ({dates, setDates, events, venues, selectedDate}) => {
 
+    const [show, setShow] = useState(false)
+    const [test, setTest] = useState({})
+
+    const descriptionRef = useRef(null)
+
     const excludedVenues = ["Standard Time", "Sounds Good", "Bambi's", "Cafeteria", "BSMT254", "The Comfort Zone"]
-    const filteredEvents = events.sort((a,b) => Date.parse(a.start_time) - Date.parse(b.start_time))
+    const filteredEvents = events.sort((a,b) => Date.parse(a?.start_time) - Date.parse(b?.start_time))
                                              .filter(i => venues.length === 1 && venues.includes("Other") ? !excludedVenues.includes(i.venue.name):
                                                     venues.length > 1 && venues.includes("Other")? !excludedVenues.includes(i.venue.name) || venues.includes(i.venue.name):
                                                     venues.length > 0? venues.includes(i.venue.name): events)
-    const filteredDates = []                                       
-
+                                       
+    const filteredDates = []  
+                                         
     useEffect(() => {
         filteredEvents.forEach(i => {
-            const eventDate = new Date(i.start_time).toDateString()
+            const eventDate = new Date(i?.start_time).toDateString()
             if (!filteredDates.includes(eventDate)) {
             filteredDates.push(eventDate)}
         })
         setDates(filteredDates)
     },[events,venues])
 
+    useEffect(() => {
+        if (descriptionRef.current !== null ) {
+            descriptionRef.current.innerHTML = test.description? test.description?.split("\n").join(`<br />`)
+            .replace(/[\u0000-\u001F\u007F-\u009F]|b\u0000/g,"").replace("&",`... <a href="${test.link}" target=_blank>Read More</a>`):""
+        }
+    },[show])
+
+    const handleShow = (obj) => {
+        setShow(true)
+        setTest(obj)
+    }
+
+    const handleClose = () => {
+        setShow(false)
+    }
+
+    console.log(dates);
+
+
     return (
      <>
         <Container className="d-flex flex-column align-items-center">
+        <Modal size="md" show={show} onHide={handleClose} >
+                <Modal.Header closeButton>{test?.name}</Modal.Header>
+                <Modal.Body>
+                    <p ref={descriptionRef}></p>
+                </Modal.Body>
+            </Modal>
         {dates.filter(j => selectedDate === ""? typeof Date.parse(j) === "number" : Date.parse(j) === selectedDate ).map( k =>  
          <ul className="px-0 " style={{listStyleType:"none"}}>
          <h3>{k}</h3>
          {filteredEvents.filter(i => k === new Date(i.start_time).toDateString()).map(i =>
           <li className="mb-2 ">
-            <Row className="d-flex justify-content-between border gx-0" style={{height:"30vh", width:"50vw"}}>
-                <Col className="h-100" xl={3}>  
+            <Row onClick={() => handleShow(i)} className="d-flex justify-content-between border gx-0" style={{height:"30vh", width:"50vw", cursor:"pointer"}}>
+                <Col className="border h-100" xl={3}>  
                     {i.thumbnail? <img className=" object-fit-cover" src={i.thumbnail} height="100%" width="100%"></img>
                                  :<i className="bi bi-images text-center mt-4"  style={{fontSize:"4rem",display:"block", margin:"auto"}}></i>}
                 </Col>
                 <Col className="ps-4" xl={9}>
-                    {i.name}
-                    <div className="mt-5">{i.start_time.split(" ")[1]} - {i.end_time?.split(" ")[1]}</div>
-                    <div>{i.venue.name}</div>
-                    <div>{i.venue.full_address}</div>
+                    <h5>{i.name}</h5>
+                    <div className="mt-5">{i.start_time?.split(" ")[1]} - {i.end_time?.split(" ")[1]}</div>
+                    <div className="fw-bold">{i.venue?.name}</div>
+                    <div>{i.venue?.full_address}</div>
 
                 </Col>
             </Row>
