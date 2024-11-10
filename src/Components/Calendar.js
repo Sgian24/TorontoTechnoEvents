cd888050c8e964f2d46a2826bdfe786ae57ebe76
Sgian24 <93100806+Sgian24@users.noreply.github.com>
2024-11-10 15:41:30 -0500

    Add styling to Calendar component.

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -2,24 +2,22 @@ import { useState, useRef } from "react";
 import DatePicker from "react-datepicker";
 import { Container, Col, Row, Button } from "react-bootstrap";
 
-const Calendar = ({dates, selectedDate, setSelectedDate}) => {
+const Calendar = ({dates, setSelectedDate}) => {
 
     const testing = useRef(null)
     const [startDate, setStartDate] = useState(new Date())
 
     const testing2 = dates.map(i => new Date(Date.parse(i)))
-
-    console.log("testing",testing2);
     
     return (
         <>
         <Container>
             <Row className="px-0" style={{width:"50vw"}}>
                 <Col className="px-0 w-25 ">
-                    <DatePicker id="calendar" ref={testing} selected={startDate} onChange={(date) => setStartDate(date)} highlightDates={testing2} inline/>
+                    <DatePicker calendarClassName="calendar" id="calendar" ref={testing} selected={startDate} onChange={(date) => setStartDate(date)} highlightDates={testing2} inline/>
                     <div>
-                        <Button className="mt-2 me-2 w-25" variant="outline-primary" onClick={() => setSelectedDate(Date.parse(startDate.toDateString()))}>Filter</Button>
-                        <Button className="mt-2 w-25" variant="outline-primary" onClick={() => setSelectedDate("")}>Show All</Button>
+                        <Button className="me-2" variant="outline-primary" style={{fontWeight:"bold", color:"black"}} onClick={() => setSelectedDate(Date.parse(startDate.toDateString()))}>Filter</Button>
+                        <Button variant="outline-primary" style={{fontWeight:"bold", color:"black"}} onClick={() => setSelectedDate("")}>Show All</Button>
                     </div>
                 </Col>
             </Row>
